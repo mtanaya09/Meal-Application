c8d97a4a5488c165ce52e7d8fdae9c1e021676f9
mtanaya09 <mtanaya09@gmail.com>
2023-01-17 12:44:46 +0800

    Styling and Theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Third App'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
