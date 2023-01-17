fdf38ac82e8f4621ad4d2805a1108cd35ce3182c
mtanaya09 <mtanaya09@gmail.com>
2023-01-17 13:15:58 +0800

    Passing data via the constructor

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  //CMS constructor
+  CategoriesMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text(
