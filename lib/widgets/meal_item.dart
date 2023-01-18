aced3b62a939321ba156a986d4b03625a00548b7
mtanaya09 <mtanaya09@gmail.com>
2023-01-18 18:07:45 +0800

    Adding a Mark as Favorite feature

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,7 +9,6 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-  final Function removeItem;
 
   MealItem({
     @required this.id,
@@ -18,7 +17,6 @@ class MealItem extends StatelessWidget {
     @required this.duration,
     @required this.complexity,
     @required this.affordability,
-    @required this.removeItem,
   });
 
   //transforming string into human readable text
@@ -62,7 +60,7 @@ class MealItem extends StatelessWidget {
     )
         .then((result) {
       if (result != null) {
-        removeItem(result);
+        // removeItem(result);
       }
     });
   }
