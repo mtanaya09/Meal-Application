dce2b36d23a27c2c2e5edcf24ba3bb4f93afdf2b
mtanaya09 <mtanaya09@gmail.com>
2023-01-18 14:33:01 +0800

    Popping pages and passing data back

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Third App',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
-        accentColor: Colors.lime,
+        accentColor: Colors.yellow,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
